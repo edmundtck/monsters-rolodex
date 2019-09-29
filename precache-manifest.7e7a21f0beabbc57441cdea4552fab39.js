8f34bc553b10df691f2ca8d1dd894ab20b917067
Edmund Teo <Edmund@edmunds-mac.mbox.com>
2019-09-29 21:52:07 +0800

    Updates

---
+++ precache-manifest.7e7a21f0beabbc57441cdea4552fab39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc93a2d68a037f3312c0d6e46517f170",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "33ab4a78520542fcdb10",
+    "url": "/monsters-rolodex/static/css/main.11e78c49.chunk.css"
+  },
+  {
+    "revision": "958f049a456bfe417d48",
+    "url": "/monsters-rolodex/static/js/2.8df7badc.chunk.js"
+  },
+  {
+    "revision": "33ab4a78520542fcdb10",
+    "url": "/monsters-rolodex/static/js/main.dd484f5d.chunk.js"
+  },
+  {
+    "revision": "f9d70e3af54ddae19106",
+    "url": "/monsters-rolodex/static/js/runtime-main.e20afc0a.js"
+  }
+]);
